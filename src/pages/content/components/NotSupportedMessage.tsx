aae6a9ca0166aafb0e7c69fc2739a4b2999d3b3a
idanrozin <idanro@ai21.com>
2023-09-25 21:08:01 +0300

    styling and minor improvement in notSupported

--- src/pages/content/components/NotSupportedMessage.tsx
+++ src/pages/content/components/NotSupportedMessage.tsx
@@ -5,17 +5,25 @@ export default function NotSupportedMessage({ close }: { close: () => void }) {
   useEffect(() => {
     const timer = setTimeout(() => {
       close();
-    }, 3000);
+    }, 10000);
     return () => {
       if (timer) {
         clearTimeout(timer);
       }
     };
-  });
+  }, []);
   return (
-    <CustomDiv>
-      {"I Don't see any PR here to approve"}
-      <button onClick={close}>Close</button>
+    <CustomDiv customStyle="flex flex-col ">
+      <div className="mb-[10px] text-xl text-black py-[20px] m-[190px] ">
+        {"I Don't see any PR here to approve"} <br />
+        {"Or You don't have the permission to approve this PR"}
+      </div>
+      <button
+        className="mx-auto w-[160px] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={close}
+      >
+        Close
+      </button>
     </CustomDiv>
   );
 }
